f6e42458a6b0d0608101ee33e2992c0e6de5220c
Luijhy Guerra <qdsjguerra@utp.edu.pe>
2024-03-01 00:27:09 -0500

    fix layouts and packaje.json

--- components/providers/convex-provider.tsx
+++ components/providers/convex-provider.tsx
@@ -1,28 +1,19 @@
-"use client"
-
-import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+"use client";
+import { ReactNode } from "react";
 import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
-import { ReactNode } from "react";
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
-export const ConvexClientProvider = (
-    {
-        children
-    }: {
-        children: ReactNode;
-    }
-) => {
-    return (
-        <ClerkProvider
-            publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
-        >
-            <ConvexProviderWithClerk
-                useAuth={useAuth}
-                client={convex}>
-                {children}
-            </ConvexProviderWithClerk>
-        </ClerkProvider>
-    )
-}
+export const ConvexClientProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+    >
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
